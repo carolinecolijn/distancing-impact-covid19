276665ec7b6b6eea9c68a0f8f950e46fd80b9b87
Sean Anderson <sean@seananderson.ca>
2020-04-07 16:56:00 -0700

    Painfully fix messed up dates and fit model

--- 02-seeiqr-fit.R
+++ 02-seeiqr-fit.R
@@ -5,9 +5,11 @@ wd <- getwd()
 setwd(here::here("selfIsolationModel", "stan"))
 source("fit_seeiqr.R")
 
-new_data <- read.csv(here::here("nCoVDailyData/CaseCounts/BC Case counts.csv")) %>%
-  dplyr::as_tibble()
+new_data <- readr::read_csv(here::here("nCoVDailyData/CaseCounts/BC Case counts.csv"))
 names(new_data)[names(new_data) == "BC"] <- "Cases"
+
+new_data$Date[71] <- "1/4/2020"
+new_data$Date[72] <- "2/4/2020"
 new_data$Date <- lubridate::dmy(new_data$Date)
 
 new_data$day <- seq_len(nrow(new_data))
@@ -17,25 +19,36 @@ new_data$daily_diffs <- c(
 )
 # TODO: fudge this for now to give same start date as bcdata
 # (which the initial conditions have been tuned to somewhat).
-daily_diffs <- dplyr::filter(new_data, Date >= "2020-03-01")$daily_diffs
 
-## Andy's code, to be integrated...
+new_data <- dplyr::filter(new_data, Date >= "2020-03-01")
+daily_diffs <- new_data$daily_diffs
+plot(daily_diffs)
+
 # Load in number of tests each day:
 # Crude for now - want to check how the numbers of cases (positive tests)
 # compare with the ones in new_data. Could scale them up perhaps.
-# load(paste0(here::here(),
-#   "/nCoVDailyData/Labdata/testsanonym.RData"))
+load(paste0(here::here(),
+  "/nCoVDailyData/Labdata/testsanonym.RData"))
 # # Only contains dataframe 'testsanonymized'
-#
-# tests_anon <- dplyr::as_tibble(testsanonymized) %>%
-#   type.convert() %>%
-#   dplyr::mutate(results_date = date(results_date))
-#
-# tests_by_day <- tests_anon %>%
-#   dplyr::group_by(results_date) %>%
-#   dplyr::count(name = "total_tests")
-# # gives dateframe of date and total_tests
-#
+tests_anon <- dplyr::as_tibble(testsanonymized) %>%
+  type.convert() %>%
+  dplyr::mutate(results_date = lubridate::date(results_date))
+
+tests_by_day <- tests_anon %>%
+  dplyr::group_by(results_date) %>%
+  dplyr::count(name = "total_tests")
+
+total_tests <- dplyr::filter(tests_by_day, results_date %in% unique(new_data$Date))$total_tests
+
+length(daily_diffs)
+length(total_tests)
+diff(new_data$Date)
+# Create some fake test numbers that match the end of the data set:
+
+plot(total_tests)
+plot(daily_diffs/total_tests)
+
+## Andy's code, to be integrated...
 # TODO: setup-dates.R explains how Andy is setting up the dates (it's mostly
 # explanations that I didn't want to clutter up here).
 # Load in the detailed case data of estimated times between people's onset of
@@ -47,3 +60,6 @@ daily_diffs <- dplyr::filter(new_data, Date >= "2020-03-01")$daily_diffs
 # delay_data <- load_tidy_delay_data()[["delay_data"]]
 
 m <- fit_seeiqr(daily_diffs)
+# m2 <- fit_seeiqr(daily_diffs, daily_tests = total_tests)
+
+setwd(wd)

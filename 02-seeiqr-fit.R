1e4d3c4a6b13e634ce83434a6389dcb7e66b95a2
Sean Anderson <sean@seananderson.ca>
2020-04-10 14:52:41 -0700

    Add sampFrac random walk and sampled vec.

--- 02-seeiqr-fit.R
+++ 02-seeiqr-fit.R
@@ -86,7 +86,46 @@ ggplot(theta_df, aes(value)) +
 ggsave(paste0("figs/sampFrac-grid-theta-posteriors.png"),
   width = 5, height = 7)
 
+# sampFrac random walk --------------------------------------------------------
+
+m_rw <- fit_seeiqr(
+  daily_diffs,
+  sampled_fraction1 = 0.1,
+  sampFrac2_type = "rw",
+  rw_sigma = 0.1,
+  sampFrac2_prior = c(0.4, 0.2),
+  fixed_f_forecast = 1.0,
+  seeiqr_model = seeiqr_model, chains = 6, cores = 400)
+m_rw$post$sampFrac2 %>% reshape2::melt() %>% as_tibble() %>%
+  rename(day = Var2) %>%
+  group_by(day) %>%
+  summarise(
+    lwr = quantile(value, probs = 0.025),
+    lwr2 = quantile(value, probs = 0.25),
+    upr = quantile(value, probs = 0.975),
+    upr2 = quantile(value, probs = 0.75),
+    med = median(value)
+  ) %>%
+  ggplot(aes(day, med)) + geom_line() +
+  geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.2) +
+  geom_ribbon(aes(ymin = lwr2, ymax = upr2), alpha = 0.9) +
+  ylab("Sampled fraction") + xlab("Days after March 14")
+ggsave(paste0("figs/sampFrac2-rw.png"), width = 5, height = 4)
+
 # -----------------------------------------------------------------------------
+
+length(daily_diffs)
+sampled_fraction_vec <- c(
+  rep(0.2, 14),
+  approx(c(15, 20), c(0.2, 0.5), xout = seq(15, 20))$y,
+  approx(c(21, 40), c(0.5, 0.3), xout = seq(21, 39))$y,
+  rep(0.3, 60)
+)
+plot(sampled_fraction_vec)
+
+m4 <- fit_seeiqr(
+  daily_diffs, chains = 6, iter = 300,
+  seeiqr_model = seeiqr_model, sampled_fraction_vec = sampled_fraction_vec)
   seeiqr_model = seeiqr_model)
 m2 <- fit_seeiqr(
   daily_diffs,

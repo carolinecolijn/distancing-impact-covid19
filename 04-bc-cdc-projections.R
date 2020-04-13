c168c9f52b489c834ad0ef796d34b52279e9b6d8
Sean Anderson <sean@seananderson.ca>
2020-04-13 15:31:22 -0700

    Update to May 1 fixed f for cdc

--- 04-bc-cdc-projections.R
+++ 04-bc-cdc-projections.R
@@ -2,6 +2,10 @@ setwd(here::here("selfIsolationModel/stan"))
 source("data-model-prep.R")
 dir.create("figs-cdc", showWarnings = FALSE)
 
+days_change_sd <- length(seq(lubridate::ymd("2020-03-01"), lubridate::ymd("2020-05-01"), by = "1 day"))
+days_change_sd
+.start <- lubridate::ymd("2020-03-01")
+
 m <- fit_seeiqr(daily_diffs, seeiqr_model = seeiqr_model, iter = 2000, chains = 8)
 saveRDS(m, file = "data-generated/main-fit-2000.rds")
 m <- readRDS("data-generated/main-fit-2000.rds")
@@ -11,14 +15,14 @@ sd_strength <- seq(0, 1, 0.2) %>% purrr::set_names()
 m_fs <- purrr::map(sd_strength, ~ {
   fit_seeiqr(
     daily_diffs,
-    fixed_f_forecast = .x,
-    seeiqr_model = seeiqr_model, chains = 8, iter = 2000
+    fixed_f_forecast = .x, day_start_fixed_f_forecast = days_change_sd,
+    seeiqr_model = seeiqr_model, chains = 8, iter = 1600
   )
 })
-saveRDS(m_fs, "data-generated/f-proj-fits.rds")
+saveRDS(m_fs, "data-generated/f-proj-fits-cdc.rds")
 
 # If coming back:
-m_fs <- readRDS("data-generated/f-proj-fits.rds")
+m_fs <- readRDS("data-generated/f-proj-fits-cdc.rds")
 purrr::walk(m_fs, ~ print(.x$fit, pars = c("R0", "f2", "phi", "sampFrac2")))
 names(m_fs)
 
@@ -27,14 +31,16 @@ ylim_c <- c(0, 4500)
 
 .coord <- coord_cartesian(
   expand = FALSE, ylim = ylim,
-  xlim = c(lubridate::ymd("2020-03-01"), lubridate::ymd("2020-05-13"))
+  xlim = c(lubridate::ymd("2020-03-01"), lubridate::ymd("2020-06-10"))
 )
 .coord_c <- coord_cartesian(
   expand = FALSE, ylim = ylim_c,
-  xlim = c(lubridate::ymd("2020-03-01"), lubridate::ymd("2020-05-13"))
+  xlim = c(lubridate::ymd("2020-03-01"), lubridate::ymd("2020-06-10"))
 )
 .theme <- theme(title = element_text(size = rel(0.9))) +
   theme(strip.text.x = element_text(angle = 0, hjust = 0))
+
+.vline <- geom_vline(xintercept = .start + days_change_sd - 1, lty = 2, alpha = 0.4)
 
 .today
 .m_fs <- m_fs
@@ -46,7 +52,7 @@ names(.m_fs)
 sc_order <- names(.m_fs)
 
 g <- make_projection_plot(.m_fs, facet = TRUE, ncol = 2, sc_order = sc_order) +
-  .theme + .coord
+  .theme + .coord + .vline
 ggsave(paste0("figs-cdc/proj-facet-", .today, ".png"),
   width = 6, height = 6, dpi = 450
 )
@@ -54,7 +60,7 @@ ggsave(paste0("figs-cdc/proj-facet-", .today, ".png"),
 g <- make_projection_plot(.m_fs,
   ylim = ylim_c, facet = TRUE, ncol = 2,
   cumulative = TRUE, sc_order = sc_order
-) + .theme + .coord_c
+) + .theme + .coord_c + .vline
 ggsave(paste0("figs-cdc/proj-cumulative-facet-", .today, ".png"),
   width = 6, height = 6, dpi = 450
 )
@@ -62,7 +68,7 @@ ggsave(paste0("figs-cdc/proj-cumulative-facet-", .today, ".png"),
 make_one_panel <- function(obj, title) {
   make_projection_plot(list(obj), ylim = ylim, points_size = 1.6) +
     ggtitle(title) +
-    .coord + .theme
+    .coord + .theme + .vline
   file_name <- gsub("%", "", gsub("[a-zA-Z :]", "", title))
   ggsave(paste0("figs-cdc/proj-", file_name, "-", .today, ".png"),
     width = 5, height = 3.25, dpi = 450
@@ -71,7 +77,7 @@ make_one_panel <- function(obj, title) {
 make_one_panel_cumulative <- function(obj, title) {
   make_projection_plot(list(obj), ylim = ylim_c, points_size = 1.6, cumulative = TRUE) +
     ggtitle(title) +
-    .coord_c + .theme
+    .coord_c + .theme + .vline
   file_name <- gsub("%", "", gsub("[a-zA-Z :]", "", title))
   ggsave(paste0("figs-cdc/proj-cumulative-", file_name, "-", .today, ".png"),
     width = 5, height = 3.25, dpi = 450
@@ -140,5 +146,5 @@ get_dat_output(.m_fs, cumulative = TRUE) %>%
   readr::write_csv(paste0("figs-cdc/proj-cumulative-60-", .today, ".csv"))
 
 if (Sys.info()[["user"]] == "seananderson") {
-  system("cp -r figs-cdc/ ~/Dropbox/bc-cdc/")
+  system("cp -r figs-cdc/ ~/Dropbox/bc-cdc-may1/")
 }

a3941c622d1e4b53b449eefb68b8586fdf0cb35e
Sean Anderson <sean@seananderson.ca>
2020-04-14 14:09:20 -0700

    Update 11-onset-date.R

--- 11-onset-date.R
+++ 11-onset-date.R
@@ -37,8 +37,8 @@ load_tidy_delay_data <- function() {
 }
 
 plot_time_to_report <- function(data,
-                                start_date_report = "2020-03-10",
-                                start_date_onset = "2020-03-05",
+                                start_date_report = "2020-02-29",
+                                start_date_onset = "2020-02-29",
                                 show_weekends = FALSE,
                                 x_axis = "reported") {
   stopifnot(x_axis %in% c("reported", "onset"))
@@ -54,14 +54,11 @@ plot_time_to_report <- function(data,
         x = reported_date,
         y = time_to_report
       )) +
-      # geom_boxplot(aes(group = reported_date),
-      #   outlier.shape = NA
-      # ) +
-      # geom_jitter(height = 0, width = 0.2, alpha = 0.4, pch = 19, col = .hist_blue) +
-      geom_bin2d() +
-      # scale_fill_viridis_c() +
+      geom_jitter(height = 0, width = 0.2, alpha = 0.4, pch = 19, col = .hist_blue) +
+      geom_boxplot(aes(group = reported_date),
+        outlier.shape = NA, fill = NA, colour = "grey30", alpha = 0.6,
+      ) +
       scale_fill_distiller(palette = "Blues", direction = 1, limits = c(1, 10)) +
-      # geom_density_2d() +
       coord_cartesian(expand = FALSE) +
       labs(
         fill = "Reported\ncases",
@@ -87,11 +84,10 @@ plot_time_to_report <- function(data,
         x = symptom_onset_date,
         y = time_to_report
       )) +
-      # geom_boxplot(aes(group = symptom_onset_date),
-      #   outlier.shape = NA
-      # ) +
-      # geom_jitter(height = 0) +
-      geom_bin2d() +
+      geom_jitter(height = 0, width = 0.22, alpha = 0.35, size = 0.9, pch = 19, col = .hist_blue) +
+      geom_boxplot(aes(group = symptom_onset_date),
+        outlier.shape = NA, fill = NA, colour = "grey30", alpha = 0.6,
+      ) +
       scale_fill_distiller(palette = "Blues", direction = 1, limits = c(1, 10)) +
       geom_abline(
         slope = -1,
@@ -114,18 +110,38 @@ plot_time_to_report <- function(data,
 }
 
 delay_data <- load_tidy_delay_data()[["delay_data"]]
-plotdelay1 <- plot_time_to_report(delay_data) + xlim(lubridate::ymd("2020-03-05"), lubridate::ymd("2020-04-06")) + guides(fill = FALSE) + theme(axis.title.y = element_blank())
-plotdelay2 <- plot_time_to_report(delay_data, x_axis = "onset") + xlim(lubridate::ymd("2020-03-05"), lubridate::ymd("2020-04-06")) + theme(legend.position = c(0.85, 0.68))  + theme(axis.title.y = element_blank())
-g <- cowplot::plot_grid(
-  plotdelay2,
-  plotdelay1,
-  ncol = 1, labels = "AUTO", label_x = 0.09, label_y = 0.95
-) +   theme(plot.margin = margin(2, 11, 2, 0))
+# plotdelay1 <- plot_time_to_report(delay_data) + xlim(lubridate::ymd("2020-03-05"), lubridate::ymd("2020-04-06")) + guides(fill = FALSE) + theme(axis.title.y = element_blank())
+plotdelay2 <- plot_time_to_report(delay_data, x_axis = "onset") + xlim(lubridate::ymd("2020-02-29"), lubridate::ymd("2020-04-06")) +
+  coord_cartesian(expand = FALSE, ylim = c(0, 32))
 
-y.grob <- grid::textGrob("Time from symptom onset to reported case (days)",
-  gp=grid::gpar(col="grey30", fontsize=11), rot=90)
+start_date_report <- "2020-02-29"
 
-png("figs-ms/onset.png", width = 4.2, height = 5.5, units = "in", res = 300)
-gridExtra::grid.arrange(gridExtra::arrangeGrob(g, left = y.grob, padding = unit(0.5, "line")))
-dev.off()
+.x <- delay_data %>%
+  filter(reported_date > start_date_report) %>%
+  group_by(reported_date) %>%
+  summarize(confirmed_cases = n())
 
+plotdelay1 <- .x %>%
+  ggplot(aes(x = reported_date, y = confirmed_cases)) +
+  geom_col(fill = .hist_blue, alpha = .8,
+    colour = "grey90", lwd = 0.15) +
+  coord_cartesian(expand = FALSE, ylim = c(0, max(.x$confirmed_cases) * 1.02)) +
+  labs(
+    fill = "Reported\ncases",
+    x = "Date of reported case",
+    y = "Reported cases"
+  ) + xlim(lubridate::ymd("2020-02-29"), lubridate::ymd("2020-04-06"))
+
+
+g <- cowplot::plot_grid(plotdelay1, plotdelay2, ncol = 1,
+  labels = "AUTO", align = "hv",  label_x = 0.145, label_y = 0.962) +
+  theme(plot.margin = margin(2, 11, 2, 0))
+ggsave("figs-ms/onset.png", width = 4.5, height = 4.7, dpi = 400)
+
+# y.grob <- grid::textGrob("Time from symptom onset to reported case (days)",
+#   gp = grid::gpar(col = "grey30", fontsize = 11), rot = 90
+# )
+
+# png("figs-ms/onset.png", width = 4.2, height = 5.5, units = "in", res = 300)
+# gridExtra::grid.arrange(gridExtra::arrangeGrob(g, left = y.grob, padding = unit(0.5, "line")))
+# dev.off()

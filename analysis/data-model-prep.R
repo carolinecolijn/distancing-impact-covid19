8c5cba78675d03b0561ce97c7f964ffa6a6e2031
Sean Anderson <sean@seananderson.ca>
2020-04-16 00:15:31 -0700

    Update data-model-prep.R

--- analysis/data-model-prep.R
+++ analysis/data-model-prep.R
@@ -7,9 +7,15 @@ rstan_options(auto_write = TRUE)
 dir.create("../data-generated", showWarnings = FALSE)
 dir.create("figs-ms", showWarnings = FALSE)
 options(mc.cores = parallel::detectCores() / 2)
-theme_set(ggsidekick::theme_sleek()) # devtools::install_github("seananderson/ggsidekick")
-source("fit_seeiqr.R")
-dat <- readr::read_csv(here::here("data-raw/BC-Case-Counts_09.04.2020.csv"))
+# remotes::install_github("seananderson/ggsidekick")
+theme_set(ggsidekick::theme_sleek())
+dat <- here::here("data-raw/BC-Case-Counts_09.04.2020.csv") %>%
+  readr::read_csv(col_types = readr::cols(
+  Date = readr::col_character(),
+  BC = readr::col_double(),
+  `ICU Census` = readr::col_double(),
+  new_icu_confirmed = readr::col_double()
+))
 names(dat)[names(dat) == "BC"] <- "Cases"
 dat$Date <- lubridate::dmy(dat$Date)
 dat$day <- seq_len(nrow(dat))
@@ -24,14 +30,14 @@ if (.today == "2020-04-08") {
   daily_diffs <- c(daily_diffs, 34, 40, 35) # April 9, 10, 11
   .today <- "2020-04-11"
 }
-seeiqr_model <- stan_model("seeiqr.stan")
+tibble(
+  date = seq(dat$Date[1], lubridate::ymd(.today), by = "1 day"),
+  cases = daily_diffs
+) %>%
+  readr::write_csv("../data-generated/daily-cases.csv")
+seeiqr_model <- rstan::stan_model("seeiqr.stan")
+source("fit_seeiqr.R")
 source("functions_sir.R")
 source("make_projection_plot.R")
 
 .hist_blue <- RColorBrewer::brewer.pal(6, "Blues")[5]
-
-tibble(
-  date = seq(dat$Date[1], lubridate::ymd("2020-04-11"), by = "1 day"),
-  cases = daily_diffs
-) %>%
-  readr::write_csv("../data-generated/daily-cases.csv")
